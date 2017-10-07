c316e79888abcd1d1df5f2f83143c6d3e0315d67
Joseph P. Short <josephpshort@gmail.com>
2017-10-07 09:53:59 -0400

    starting unit tests for presentation layer

---
+++ F23.PresentationModelLnL.Presentation.Tests/CaseSheetPresentationFactoryTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using F23.PresentationModelLnL.Contracts.Repositories;
+using F23.PresentationModelLnL.Domain.CaseSheets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace F23.PresentationModelLnL.Presentation.Tests
+{
+    [TestClass]
+    public class CaseSheetPresentationFactoryTests
+    {
+        [TestMethod]
+        public async Task GetCaseSheetDetailsAsync_HappyPath()
+        {
+            // Arrange
+            var caseSheetDetails = new CaseSheetDetails
+            {
+                Id = 1,
+                CaseDate = new DateTime(2017, 1, 1),
+                CaseSheetNumber = "CS-1",
+                IsProcessed = false,
+                LocationId = 1,
+                LocationName = "Test Location",
+                TotalCost = 50m
+            };
+            var mockRepo = new Mock<ICaseSheetRepository>();
+            mockRepo.Setup(x => x.GetCaseSheetDetailsAsync(1)).ReturnsAsync(caseSheetDetails);
+            var factory = new CaseSheets.CaseSheetPresentationFactory(mockRepo.Object);
+
+            // Act
+            var result = await factory.GetCaseSheetDetailsAsync(caseSheetDetails.Id, false);
+
+            // Assert
+            // TODO.JS: case sheet products
+            Assert.AreEqual(caseSheetDetails, result.CaseSheetDetails);
+            Assert.IsFalse(result.CanDelete);
+            Assert.IsFalse(result.CanEdit);
+        }
+    }
+}
